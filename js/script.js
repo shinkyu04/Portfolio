7e59fcec812ed99189c2f5d0cdc405d17ad2f0b7
shinkyu04 <joshuaresarre561@gmail.com>
2026-02-19 13:39:21 +0800

    Add back-to-top button that appears when user scroll down and it's function that scroll to the very top when clicked

--- js/script.js
+++ js/script.js
@@ -25,3 +25,34 @@ function autoResize(textarea) {
 // Get the current year and inject it into the span
 const yearSpan = document.querySelector('#current-year');
 yearSpan.textContent = new Date().getFullYear();
+
+// JScript for back to top button appear and disappear
+const backToTop = document.querySelector('.arrow');
+
+window.addEventListener('scroll', () => {
+    // If we have scrolled more than 300px from the top
+    if (window.scrollY > 70) {
+        backToTop.classList.add('show');
+    } else {
+        backToTop.classList.remove('show');
+    }
+
+  // If you are at the very top (0), make it vanish instantly
+    if (window.scrollY <= 10) {
+        backToTop.style.transition = "none";
+        // Reset it after a tiny delay so it can animate next time
+        setTimeout(() => {
+            backToTop.style.transition = "";
+        }, 10);
+    }
+});
+
+// JScript for back to top button sroll effect
+const backToTopButton = document.querySelector('.arrow');
+
+backToTopButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // This makes it glide instead of jumping
+    });
+});
